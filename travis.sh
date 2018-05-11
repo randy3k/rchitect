45410ec641222ccf1ee8199760a3a54df0045782
Randy Lai <randy.cs.lai@gmail.com>
2018-05-11 14:57:44 -0400

    update travis.sh

--- travis.sh
+++ travis.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+get_package() {
+    package="$1"
+    ver="$2"
+    wget -qO- https://cran.rstudio.com/bin/linux/ubuntu/$(lsb_release -s -c)/ | sed -n "s/.*\(${package}\)_\($ver-[a-z0-9]*\).*/\1=\2/p" | tail -1
+}
 
 InstallR() {
     sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
@@ -9,25 +14,10 @@ InstallR() {
     sudo apt-get update -qq -y
     if [ -z "$1" ]; then
         sudo apt-get install git r-base r-base-dev r-recommended -y
-    else
-        RVER="$1"
-        if [ "$RVER" == "3.5.0" || "$RVER" == "3.5" ]; then
-            VERFLAG=""
-        elif [ "$RVER" == "3.4.4" ]; then
-            VERFLAG="=3.4.4-1trusty0"
-        elif [ "$RVER" == "3.4.3" ]; then
-            VERFLAG="=3.4.3-1trusty0"
-        elif [ "$RVER" == "3.4.2" ]; then
-            VERFLAG="=3.4.2-2trusty"
-        elif [ "$RVER" == "3.4.1" ]; then
-            VERFLAG="=3.4.1-2trusty0"
-        elif [ "$RVER" == "3.4.0" ]; then
-            VERFLAG="=3.4.0-1trusty0"
-        fi
-        echo download R version "$1"
-        sudo apt-get install git r-base"$VERFLAG" -y
-        sudo apt-get install git r-base-dev"$VERFLAG" -y
-        sudo apt-get install git r-recommended"$VERFLAG" -y
+    elser
+        sudo apt-get install git $(get_package r-base "$1") -y
+        sudo apt-get install git $(get_package r-base-dev "$1") -y
+        sudo apt-get install git $(get_package r-recommended "$1") -y
     fi
 }
 
