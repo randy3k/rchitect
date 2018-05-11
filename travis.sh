c7c3e5f8680186f7c2ec70cede6f8d5d0832199c
Randy Lai <randy.cs.lai@gmail.com>
2018-05-11 15:35:50 -0400

    revert .travis.yml

--- travis.sh
+++ travis.sh
@@ -12,15 +12,15 @@ InstallR() {
     sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
     sudo add-apt-repository -y "deb http://cran.rstudio.com/bin/linux/ubuntu $(lsb_release -s -c)/"
     sudo apt-get update -qq -y
-    if [ -z "$1" ]; then
-        sudo apt-get install git r-base r-base-dev r-recommended -y
-    elser
-        sudo apt-get install git $(get_package r-base "$1") -y
-        sudo apt-get install git $(get_package r-base-dev "$1") -y
-        sudo apt-get install git $(get_package r-recommended "$1") -y
+    if [ "$1" == "r-release" ]; then
+        sudo apt-get install r-base r-base-core r-recommended -y
+    else
+        sudo apt-get install -y \
+            $(get_package r-base  "$1") \
+            $(get_package r-base-core  "$1") \
+            $(get_package r-recommended "$1")
     fi
 }
-
 
 COMMAND=$1
 shift
