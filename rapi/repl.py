c7c6973ba744a53dd630544ce0979c54012fa86c
Randy Lai <randy.cs.lai@gmail.com>
2018-05-29 11:23:50 -0400

    process events in repl

--- rapi/repl.py
+++ rapi/repl.py
@@ -2,13 +2,24 @@
 
 from .interface import rprint, reval_with_visible
 import sys
-from prompt_toolkit import prompt
+from prompt_toolkit.shortcuts import prompt, create_eventloop
+from .interface import process_events
+
+
+__all__ = ["repl_r"]
+
+
+def inputhook(context):
+    while True:
+        if context.input_is_ready():
+            break
+        process_events()
 
 
 def repl_r():
     while True:
         try:
-            text = prompt("> ")
+            text = prompt("> ", eventloop=create_eventloop(inputhook=inputhook))
             result = reval_with_visible(text)
             if result["visible"]:
                 rprint(result["value"])
