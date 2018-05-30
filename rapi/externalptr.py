03092b2618444761eb93f2e2baa1b7641b61cec9
Randy Lai <randy.cs.lai@gmail.com>
2018-05-29 22:31:12 -0400

    use pythonapi to protect pyobject

--- rapi/externalptr.py
+++ rapi/externalptr.py
@@ -1,5 +1,5 @@
-from ctypes import py_object, c_void_p, cast, pointer, CFUNCTYPE
-from .internals import R_MakeExternalPtr, R_RegisterCFinalizerEx, R_NilValue
+from ctypes import py_object, c_void_p, cast, pointer, CFUNCTYPE, POINTER, pythonapi
+from .internals import R_MakeExternalPtr, R_ExternalPtrAddr, R_RegisterCFinalizerEx, R_NilValue
 from .types import SEXP
 
 extptrs = {}
@@ -7,13 +7,14 @@ extptrs = {}
 
 @CFUNCTYPE(None, SEXP)
 def finalizer(s):
-    if s.value in extptrs:
-        del extptrs[s.value]
+    ptr = cast(R_ExternalPtrAddr(s), POINTER(py_object))
+    fpy = ptr.contents
+    pythonapi.Py_DecRef(fpy)
 
 
 def rextptr(f):
     fpy = py_object(f)
+    pythonapi.Py_IncRef(fpy)
     s = R_MakeExternalPtr(cast(pointer(fpy), c_void_p), R_NilValue, R_NilValue)
-    extptrs[s.value] = fpy
     R_RegisterCFinalizerEx(s, finalizer, 1)
     return s
