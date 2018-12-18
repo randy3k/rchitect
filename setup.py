71bd3624ffc2b78a51f729c78061c905543137cb
Randy Lai <randy.cs.lai@gmail.com>
2018-12-18 15:30:14 -0500

    exclude tests module

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     description='Interoperate R with Python',
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
-    packages=find_packages('.'),
+    packages=find_packages('.', exclude=["tests"]),
     install_requires=[
         "multipledispatch"
     ],
