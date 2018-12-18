4f1ac37139f54b828c09a4de9a08a865543cef96
Randy Lai <randy.cs.lai@gmail.com>
2018-12-18 15:31:07 -0500

    bump to 0.2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# v0.2.1
+
+  - exclude tests module
+
 # v0.2.0
 
   - rebranded as `rchitect`
--- rchitect/__init__.py
+++ rchitect/__init__.py
@@ -16,7 +16,7 @@ __all__ = [
     "robject"
 ]
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 
 def start(
