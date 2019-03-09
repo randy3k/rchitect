ba55b74ff578d0a739d2ffcad814d1b980e4db93
Randy Lai <randy.cs.lai@gmail.com>
2019-03-09 18:51:43 -0500

    remove tests

--- tests/test_callback.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from rchitect import rcopy, rcall, robject
-
-
-def test_lambda():
-    rcopy(rcall(robject(lambda x: x + 3), 4)) == 7
