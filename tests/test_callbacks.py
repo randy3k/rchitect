7bc3d952a8c86929a993d87dd2133c757fa63919
Randy Lai <randy.cs.lai@gmail.com>
2019-04-14 00:26:35 -0400

    remove test_yes_no_cancel

--- tests/test_callbacks.py
+++ tests/test_callbacks.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from rchitect import reval, rcopy, rcall
+from rchitect import reval, rcopy
 
 
 def test_read_console(mocker):
@@ -15,17 +15,3 @@ def test_read_console_long(mocker):
     mocker.patch("rchitect.setup.ask_input", return_value=s)
     ret = reval("readline('> ')")
     assert rcopy(ret) == s
-
-
-def test_yes_no_cancel(mocker):
-    mocker.patch("rchitect.setup.ask_input", return_value="y")
-    ret = reval("askYesNo('Yes/No/Cancel')")
-    assert rcopy(ret) is True
-
-    mocker.patch("rchitect.setup.ask_input", return_value="n")
-    ret = reval("askYesNo('Yes/No/Cancel')")
-    assert rcopy(ret) is False
-
-    mocker.patch("rchitect.setup.ask_input", return_value="c")
-    ret = reval("askYesNo('Yes/No/Cancel')")
-    assert rcall("is.na", ret, _convert=True)
