0680002e8451e6637d5bcb84957291e861c7b8ef
Randy Lai <randy.cs.lai@gmail.com>
2019-04-14 20:49:10 -0400

    more callback tests

--- tests/test_callbacks.py
+++ tests/test_callbacks.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from rchitect import reval, rcopy
+from rchitect._cffi import lib, ffi
+import pytest
 
 
 def test_read_console(mocker):
@@ -15,3 +17,41 @@ def test_read_console_long(mocker):
     mocker.patch("rchitect.setup.ask_input", return_value=s)
     ret = reval("readline('> ')")
     assert rcopy(ret) == s
+
+
+def test_read_console_interrupt(mocker):
+    mocker.patch("rchitect.setup.ask_input", side_effect=KeyboardInterrupt())
+    with pytest.raises(Exception) as excinfo:
+        reval("readline('> ')")
+    assert "eval error" in str(excinfo)
+
+
+def test_yes_no_cancel(mocker):
+    for (a, v) in [('y', 1), ('n', 2), ('c', 0)]:
+        mocker.patch("rchitect.setup.ask_input", return_value=a)
+        ret = lib.cb_yes_no_cancel(ffi.new("char[10]", b"> "))
+        assert ret == v
+    mocker.resetall()
+
+
+def test_yes_no_cancel_exceptions(mocker):
+    count = [0]
+
+    def throw_on_first_run(_):
+        if count[0] == 0:
+            count[0] += 1
+            raise Exception()
+        else:
+            return "y"
+
+    mocker.patch("rchitect.setup.ask_input", side_effect=throw_on_first_run)
+    ret = lib.cb_yes_no_cancel(ffi.new("char[10]", b"> "))
+    assert ret == 1
+
+    mocker.patch("rchitect.setup.ask_input", side_effect=EOFError())
+    ret = lib.cb_yes_no_cancel(ffi.new("char[10]", b"> "))
+    assert ret == 0
+
+    mocker.patch("rchitect.setup.ask_input", side_effect=KeyboardInterrupt())
+    ret = lib.cb_yes_no_cancel(ffi.new("char[10]", b"> "))
+    assert ret == 0
