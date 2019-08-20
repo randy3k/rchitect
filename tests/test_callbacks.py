fb60c1124b61d9a46bdcbbed3230df7b9d0517db
Randy Lai <randy.cs.lai@gmail.com>
2019-08-20 12:51:17 -0700

    fix python 3 error

--- tests/test_callbacks.py
+++ tests/test_callbacks.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from rchitect import reval, rcopy
 from rchitect._cffi import lib, ffi
 import pytest
+import sys
 
 
 def test_read_console(mocker):
@@ -23,7 +24,10 @@ def test_read_console_interrupt(mocker):
     mocker.patch("rchitect.setup.ask_input", side_effect=KeyboardInterrupt())
     with pytest.raises(Exception) as excinfo:
         reval("readline('> ')")
-    assert "eval error" in str(excinfo)
+    if sys.version_info[0] >= 3:
+        assert "eval error" in str(excinfo.value)
+    else:
+        assert "eval error" in str(excinfo)
 
 
 def test_yes_no_cancel(mocker):
