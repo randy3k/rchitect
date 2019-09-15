c87606836eb096797ca6a3b9a8c742d12203eace
Randy Lai <randy.cs.lai@gmail.com>
2019-09-15 15:24:56 -0700

    add completion utils

---
+++ rchitect/completion.py
@@ -0,0 +1,30 @@
+from rchitect.interface import rcall, reval, rcopy, rstring, rint
+from six import text_type
+
+
+def assign_line_buffer(buf):
+    rcall(reval("utils:::.assignLinebuffer"), rstring(buf))
+    rcall(reval("utils:::.assignEnd"), rint(len(buf)))
+    token = rcopy(text_type, rcall(reval("utils:::.guessTokenFromLine")))
+    return token
+
+
+def complete_token(timeout=0):
+    try:
+        if timeout:
+            rcall(("base", "setTimeLimit"), timeout)
+        reval("utils:::.completeToken()")
+        if timeout:
+            rcall(("base", "setTimeLimit"))
+    except Exception:
+        if timeout:
+            rcall(("base", "setTimeLimit"))
+            rcall(("base", "assign"), "comps", None, env=reval("utils:::.CompletionEnv"))
+
+
+def retrieve_completions():
+    completions = rcopy(list, rcall(reval("utils:::.retrieveCompletions")))
+    if not completions:
+        return []
+    else:
+        return completions
