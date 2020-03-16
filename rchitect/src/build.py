28c06dfd86fa7d4bcaf6af6584ad071df528fc60
Randy Lai <randy.cs.lai@gmail.com>
2020-03-16 16:07:36 -0700

    fix cffi warnings

--- rchitect/src/build.py
+++ rchitect/src/build.py
@@ -12,22 +12,22 @@ cb_cdef_pattern = re.compile("// begin cb cdef([^$]*)// end cb cdef")
 for header_file in ["R.h", "libR.h", "gli.h", "parse.h", "process_events.h"]:
     with open(os.path.join(cwd, header_file), "r") as f:
         m = cdef_pattern.search(f.read(), re.M)
-        ffibuilder.cdef(m.group(1).replace("RAPI_EXTERN", ""))
+        ffibuilder.cdef(m.group(1).replace("RAPI_EXTERN", "extern"))
 
 if sys.platform.startswith("win"):
     ffibuilder.cdef("""
-        char *(*get_R_HOME)(void);
-        char *(*getRUser)(void);
-        int* UserBreak_t;
-        int* CharacterMode_t;
-        int* EmitEmbeddedUTF8_t;
+        extern char *(*get_R_HOME)(void);
+        extern char *(*getRUser)(void);
+        extern int* UserBreak_t;
+        extern int* CharacterMode_t;
+        extern int* EmitEmbeddedUTF8_t;
     """)
 else:
     ffibuilder.cdef("""
-        void* (*R_InputHandlers);
-        void* (*R_checkActivity)(int usec, int ignore_stdin);
-        void (*R_runHandlers)(void* handlers, void* mask);
-        int* R_interrupts_pending_t;
+        extern void* (*R_InputHandlers);
+        extern void* (*R_checkActivity)(int usec, int ignore_stdin);
+        extern void (*R_runHandlers)(void* handlers, void* mask);
+        extern int* R_interrupts_pending_t;
     """)
 
 with open(os.path.join(cwd, "libR.h"), "r") as f:
