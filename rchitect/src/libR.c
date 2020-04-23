c45bce6c3f0b0be77ce634a37502efc8af8ee676
Randy Lai <randy.cs.lai@gmail.com>
2020-04-23 02:04:55 -0700

    these are not necessary

--- rchitect/src/libR.c
+++ rchitect/src/libR.c
@@ -568,21 +568,6 @@ static int load_ga_symbol(const char* name, void** ppSymbol) {
     if (*ppSymbol != NULL) {
         return 1;
     }
-    #ifndef MS_WIN64
-    if (((size_t)name & ~0xFFFF) == 0) {
-        return 0;
-    }
-    mangled_name = alloca(strlen(name) + 1 + 1 + 1 + 3);
-    if (!mangled_name)
-        return NULL;
-    for (i = 0; i < 32; ++i) {
-        sprintf(mangled_name, "_%s@%d", name, i*4);
-        *ppSymbol = (void*) GetProcAddress((HINSTANCE) libRga_t, mangled_name);
-        if (*ppSymbol != NULL) {
-            return 1;
-        }
-    }
-    #endif
     return 0;
 }
 
