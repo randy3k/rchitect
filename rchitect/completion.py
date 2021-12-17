2960421715ede26afe595068667fbec85d5294be
Randy Lai <randy.cs.lai@gmail.com>
2021-12-17 00:56:05 -0800

    complete code has already been tryCatch'ed

--- rchitect/completion.py
+++ rchitect/completion.py
@@ -1,17 +1,17 @@
 from __future__ import unicode_literals
 
-from rchitect.interface import rcall, reval, rcopy, rstring, rint, new_env
+from rchitect.interface import rcall, reval, rcopy, new_env
 from six import text_type
 
 
 def assign_line_buffer(buf):
-    rcall(reval("utils:::.assignLinebuffer"), rstring(buf))
-    rcall(reval("utils:::.assignEnd"), rint(len(buf)))
+    rcall(reval("utils:::.assignLinebuffer"), buf)
+    rcall(reval("utils:::.assignEnd"), len(buf))
     token = rcopy(text_type, rcall(reval("utils:::.guessTokenFromLine")))
     return token
 
 
-CompleteCode = """
+code = """
 tryCatch(
     {{
         if ({settimelimit}) base::setTimeLimit({timeout})
@@ -27,17 +27,12 @@ tryCatch(
 
 
 def complete_token(timeout=0):
-    try:
-        reval(
-            CompleteCode.format(
-                settimelimit="TRUE" if timeout > 0 else "FALSE",
-                timeout=str(timeout)),
-            envir=new_env()
-        )
-    except Exception:
-        if timeout:
-            rcall(("base", "setTimeLimit"))
-            rcall(("base", "assign"), "comps", None, env=reval("utils:::.CompletionEnv"))
+    reval(
+        code.format(
+            settimelimit="TRUE" if timeout > 0 else "FALSE",
+            timeout=str(timeout)),
+        envir=new_env()
+    )
 
 
 def retrieve_completions():
