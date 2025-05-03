1e6a95df7860def2daad0d86ef5907b8e1c746a3
Randy Lai <randy.cs.lai@gmail.com>
2025-05-02 23:49:08 -0700

    switch to pyproject.toml (#38)
    * switch to pyproject.toml and fix actions

--- rchitect/build.py
+++ rchitect/build.py
@@ -2,20 +2,24 @@ import os
 import re
 import sys
 from cffi import FFI
+
 ffibuilder = FFI()
 
 # cwd = os.path.dirname(os.path.realpath(__file__))
 cdef_pattern = re.compile("// begin cdef([^$]*)// end cdef")
 cb_cdef_pattern = re.compile("// begin cb cdef([^$]*)// end cb cdef")
 
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
 
 for header_file in ["R.h", "libR.h", "gil.h", "parse.h", "process_events.h"]:
-    with open(os.path.join("rchitect", "src", header_file), "r") as f:
+    with open(os.path.join(BASEDIR, "_cffi", header_file), "r") as f:
         m = cdef_pattern.search(f.read(), re.M)
         ffibuilder.cdef(m.group(1).replace("RAPI_EXTERN", "extern"))
 
 if sys.platform.startswith("win"):
-    ffibuilder.cdef("""
+    ffibuilder.cdef(
+        """
         extern char *(*get_R_HOME)(void);
         extern char *(*getRUser)(void);
         extern int* UserBreak_t;
@@ -23,23 +27,30 @@ if sys.platform.startswith("win"):
         extern int* EmitEmbeddedUTF8_t;
         extern int (*GA_peekevent)(void);
         extern int (*GA_initapp)(int, char **);
-    """)
+    """
+    )
 else:
-    ffibuilder.cdef("""
+    ffibuilder.cdef(
+        """
         extern void* R_InputHandlers;
         extern void (**R_PolledEvents_t)(void);
         extern void* (*R_checkActivity)(int usec, int ignore_stdin);
         extern void (*R_runHandlers)(void* handlers, void* mask);
         extern int* R_interrupts_pending_t;
-    """)
+    """
+    )
 
-with open(os.path.join("rchitect", "src", "libR.h"), "r") as f:
+with open(os.path.join(BASEDIR, "_cffi", "libR.h"), "r") as f:
     m = cb_cdef_pattern.search(f.read(), re.M)
-    ffibuilder.cdef("""
+    ffibuilder.cdef(
+        """
         extern "Python+C" {{
             {}
         }}
-    """.format(m.group(1)))
+    """.format(
+            m.group(1)
+        )
+    )
 
 ffibuilder.set_source(
     "rchitect._cffi",
@@ -49,13 +60,12 @@ ffibuilder.set_source(
     # include "parse.h"
     # include "process_events.h"
     """,
-    include_dirs=['rchitect/src'],
+    include_dirs=[os.path.join(BASEDIR, "_cffi")],
     sources=[
-        'rchitect/src/libR.c',
-        'rchitect/src/gil.c',
-        'rchitect/src/parse.c',
-        'rchitect/src/process_events.c'
-    ])
+        os.path.join("rchitect", "_cffi", f)
+        for f in ["libR.c", "gil.c", "parse.c", "process_events.c"]
+    ],
+)
 
 if __name__ == "__main__":
     ffibuilder.compile(verbose=True)
--- setup.py
+++ setup.py
@@ -1,54 +1,5 @@
-import os
-import re
-from setuptools import setup, find_packages
-
-
-def get_long_description():
-    with open('README.md', 'rb') as f:
-        desc = f.read().decode('utf-8')
-
-    return desc
-
-
-def get_version(package):
-    """
-    Return package version as listed in `__version__` in `__init__.py`.
-    """
-    path = os.path.join(os.path.dirname(__file__), package, '__init__.py')
-    with open(path, 'rb') as f:
-        init_py = f.read().decode('utf-8')
-    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
-
-
-install_requires = [
-    'cffi>=1.10.0',
-    'six>=1.9.0',
-    'packaging>=23.0'
-]
+from setuptools import setup
 
 setup(
-    name='rchitect',
-    author='Randy Lai',
-    author_email="randy.cs.lai@gmail.com",
-    version=get_version("rchitect"),
-    url='https://github.com/randy3k/rchitect',
-    description='Mapping R API to Python',
-    long_description=get_long_description(),
-    long_description_content_type="text/markdown",
-    packages=find_packages('.', exclude=["tests"]),
-    package_data={'rchitect': ['reticulate/*.R', 'src/*']},
-    cffi_modules=["build.py:ffibuilder"],
-    python_requires='>=3.7',
-    install_requires=install_requires,
-    setup_requires=[
-        'cffi>=1.10.0',
-        "pytest-runner"
-    ],
-    extras_require={
-        "test": [
-            "pytest",
-            "pytest-mock",
-            "pytest-cov"
-        ]
-    }
+    cffi_modules=["rchitect/build.py:ffibuilder"]
 )
